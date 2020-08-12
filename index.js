764e274b1ef215c9ba9220318ac53f5f5210649c
NDCT94 <nathan.dumas01@gmail.com>
2020-08-12 14:17:08 -0400

    mvp done, might try stretch

--- index.js
+++ index.js
@@ -94,8 +94,10 @@ Your function should accept:
 For example, getFlavorByIndex(originalFlavors, 2) would return "Black Walnut", assuming Rainbow Sherbert has been added successfully. */
 
 function getFlavorByIndex(arr, index){
-    
+    return arr[index];
 }
+
+console.log(getFlavorByIndex(originalFlavors, 2));
 
 /* Task 5: As corporate wants to add more and more flavors to their lineup, they've realized that they need to remove flavors based on flavor name, as opposed to just arbitrarily removing the first or last flavor. Your task is to get an index by flavor name, and remove that flavor from the array. 
 
@@ -110,11 +112,16 @@ Hint: You can use .splice() for this
 
 */
 
-function removeFlavorByName(/*code here*/){
+function removeFlavorByName(arr, flav){
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] === (flav)){
+            arr.splice(i,1);
+        }
+    }
+    return originalFlavors;
+};
 
-    /*code here*/
-
-}
+console.log(removeFlavorByName(originalFlavors, "Vanilla"));
 
 
 /* Task 6: With all of these changes going on, we don't want to lose track of the actual, original 31 flavors. Write a function called copy that makes a copy of the array. 
@@ -125,11 +132,12 @@ Your function should accept:
 
 and should return a new array that is identical to the old array. You can name the new array however you'd like. */
 
-function copy(/*code here*/){
+function copy(arr){
+    originalFlavorsCopy = [...arr];
+    return originalFlavorsCopy;
+};
 
-    /*code here*/
-
-}
+console.log(copy(originalFlavors));
 
 /* Task 7: July 7th is "World Chocolate Day" and Baskin Robins wants to create promotional materials highlighting all of their chocolate flavors. Write a function that checks every item in the array for a given string and returns a new array called filteredArray with just these values. Rather than hardcoding "chocolate" into your function, pass a string as a parameter, and invoke with the argument "chocolate". This way you could also filter for "Vanilla", "Sherbert", etc. when those holidays roll around.
 
@@ -146,12 +154,17 @@ DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem.
 
 hint - you can use the .includes method to help you solve this */
 
-function filterByWord(/*code here*/){
-
-    /*code here*/
-
+function filterByWord(arr, string){
+    let newArr = [];
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i].includes(string)){
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
 }
 
+console.log(filterByWord(originalFlavors, "Chocolate"));
 
 
 /* 🧁🍦🍨 STRETCH 🍨🍦🍫*/ 
